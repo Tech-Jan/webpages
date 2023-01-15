2eb52bd13f52b25e580a27f0da17d74c199dea66
tech-jan <trewej@hotmail.com>
2023-01-15 14:18:06 +0100

    added direction angle turn and instant move feature

--- sea/move.js
+++ sea/move.js
@@ -1,7 +1,30 @@
 
-
-
 jQuery(document).on("move click","body",function(e){
+
+function getRotationAngle(target)
+{
+  const obj = window.getComputedStyle(target, null);
+  const matrix = obj.getPropertyValue('-webkit-transform') ||
+    obj.getPropertyValue('-moz-transform') ||
+    obj.getPropertyValue('-ms-transform') ||
+    obj.getPropertyValue('-o-transform') ||
+    obj.getPropertyValue('transform');
+
+  let angle = 0;
+
+  if (matrix !== 'none')
+  {
+    const values = matrix.split('(')[1].split(')')[0].split(',');
+    const a = values[0];
+    const b = values[1];
+    angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+  }
+
+  return (angle < 0) ? angle +=360 : angle;
+}
+
+console.log(getRotationAngle(document.getElementById('whale')))
+
 let box = document.querySelector('.topbox');
 let whale = document.querySelector('.whale');
 let width = box.offsetWidth;
@@ -14,7 +37,20 @@ var rect = whale.getBoundingClientRect();
 var newposX = x  ;
 var newposY = y   ;
 var test = newposX-rect.right +width/2 -+ $scrollleft;
-var transform = 1;
+var test2 = rect.right - width/2 + $scrollleft - 16;
+var test3 = rect.bottom - height/2 + $scrolltop;
+var transform = 0;
+var time = Math.abs(test)/10;
+var start = 0;
+
+if (test<0) {transform=180}
+
+if ( transform != getRotationAngle(document.getElementById('whale'))) {$(".whale").css({'transform':'translateX('+test2+'px) translateY('+test3+'px) rotateY('+transform+'deg)',
+            'transition': 'transform 1s'
+			})}
+
+if (transform != getRotationAngle(document.getElementById('whale'))) {start=1001}
+
 if (newposX>430) {newposY=-30}
 if (newposX>540) {newposY=-10}
 if (newposX>810) {newposX=810}
@@ -24,8 +60,12 @@ if (newposX<-430) {newposX=-430}
 if (newposY< -105) {newposY=-105}
 if (newposY> 43) {newposY=43}
 
-$(".whale").css({'transform':'translateX('+newposX+'px) translateY('+newposY+'px)'
-			});
+
+function move() {$(".whale").css({'transform':'translateX('+newposX+'px) translateY('+newposY+'px) rotateY('+transform+'deg)',
+            'transition': 'transform '+time+'s'
+			})}
+
+setTimeout(move, start);
 
 });
 
