eaca971a43765546def11e431a2af41f5b62f8c1
tech-jan <trewej@hotmail.com>
2023-01-15 01:07:15 +0100

    mywhale with css

---
+++ sea/move.js
@@ -0,0 +1,25 @@
+
+
+
+jQuery(document).on("click","body",function(e){
+let box = document.querySelector('.topbox');
+let width = box.offsetWidth;
+let height = box.offsetHeight;
+var $scrollleft = $(document).scrollLeft()
+var $scrolltop = $(document).scrollTop()
+var x = e.clientX -width/2 + $scrollleft;
+var y = e.clientY -height/2 +$scrolltop;
+var newposX = x  ;
+var newposY = y   ;
+if (newposX>430) {newposY=-30}
+if (newposX>540) {newposY=-10}
+if (newposX>810) {newposX=810}
+if (newposX<-21 && newposY<-70 ) {newposY=-70}
+if (newposX<-214) {newposY=-57}
+if (newposX<-430) {newposX=-430}
+if (newposY< -105) {newposY=-105}
+if (newposY> 43) {newposY=43}
+$(".whale").css({'transform':'translateX('+newposX+'px) translateY('+newposY+'px) rotateZ(0deg)',
+			'borderWidth': '0 5px 5px 0',});
+});
+
